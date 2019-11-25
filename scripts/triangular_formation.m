53a54d74b220e048c8f59de20d2678ecd97b0c27
Raj Patel <rpatelpj@gmail.com>
2019-11-25 16:24:20 -0500

    Add origin shift in triangular_formation.m

--- scripts/triangular_formation.m
+++ scripts/triangular_formation.m
@@ -8,6 +8,9 @@ N = 3;
 initial_position = [   0.5,     0.7,     0.3;
                     0.1732, -0.1732, -0.1732;
                      pi./2,   pi./2,   pi./2];
+dx = -1; dy = -0.2; % Distance from origin
+initial_position(1, :) = initial_position(1, :) + dx;
+initial_position(2, :) = initial_position(2, :) + dy;
 r = Robotarium('NumberOfRobots', N, 'ShowFigure', true, 'InitialConditions', initial_position);
 
 % Select the number of iterations for the experiment
@@ -40,8 +43,8 @@ hold on; tri = patch(initial_position(1, :), initial_position(2, :), 'y');
 
 % Plot trajectory
 theta = linspace(0, 2.*pi, iterations);
-xp = 0.5*cos(theta);
-yp = 0.5*sin(theta);
+xp = 0.5*cos(theta) + dx;
+yp = 0.5*sin(theta) + dy;
 plot(xp, yp);
 
 %% Run
@@ -52,9 +55,9 @@ for t = 1:iterations
     
     % Compute trajectory
     theta = (t/iterations).*2.*pi;
-    % xp = 0.5.*cos(theta);
-    % yp = 0.5.*sin(theta);
-    % gTO = newCenter(xp, yp, theta, false);
+    % xp = 0.5.*cos(theta) + dx;
+    % yp = 0.5.*sin(theta) + dy;
+    % gTO = HC(xp, yp, theta, false);
     % gAO = gTO*gAT; dAO = gAO(1:2, 3);                       
     % gBO = gTO*gBT; dBO = gBO(1:2, 3);
     % gCO = gTO*gCT; dCO = gCO(1:2, 3);
@@ -65,24 +68,22 @@ for t = 1:iterations
     thetadot = 2.*pi/iterations;
     xdot = -0.5.*thetadot.*sin(theta);
     ydot = 0.5.*thetadot.*cos(theta);
-    gTOdot = HC(xdot, ydot, theta, true);
-    gAOdot = gTOdot*gAT; vA = gAOdot(1:2, 3);
-    gBOdot = gTOdot*gBT; vB = gBOdot(1:2, 3);
-    gCOdot = gTOdot*gCT; vC = gCOdot(1:2, 3);
+    gTTndot = HC(xdot, ydot, theta, true);
+    gATndot = gTTndot*gAT; vA = gATndot(1:2, 3);
+    gBTndot = gTTndot*gBT; vB = gBTndot(1:2, 3);
+    gCTndot = gTTndot*gCT; vC = gCTndot(1:2, 3);
     A_cum = A_cum + vA;
     B_cum = B_cum + vB;
     C_cum = C_cum + vC;
-    % tri.Vertices(1, :) = tri.Vertices(1, :) + vA';
-    % tri.Vertices(2, :) = tri.Vertices(2, :) + vB';
-    % tri.Vertices(3, :) = tri.Vertices(3, :) + vC';
+    
+    % Update triangle visual
+    tri.Vertices(1, :) = tri.Vertices(1, :) + vA';
+    tri.Vertices(2, :) = tri.Vertices(2, :) + vB';
+    tri.Vertices(3, :) = tri.Vertices(3, :) + vC';
 
     while(~init_checker(x, [A_cum, B_cum, C_cum; [x(3, 1), x(3, 2), x(3, 3)]]))
         % Get recent poses from the Robotarium
         x = r.get_poses();
-
-        % Update triangle visual
-        tri.Vertices(:, 1) = x(1, :)';
-        tri.Vertices(:, 2) = x(2, :)';
 
         % Set velocities of agents 1,...,N
         dxu = si_to_uni_dynamics([vA, vB, vC], x);
@@ -100,10 +101,6 @@ r.debug();
 function g = HC(x, y, theta, vel)
     if (vel)
         global thetadot;
-        II = [0, 1;
-              -1, 0];
-        r = [cos(theta), -sin(theta);
-             sin(theta),  cos(theta)];
         dr = [-sin(theta), -cos(theta);
               cos(theta),  -sin(theta)];
         g = [dr.*thetadot, [x; y]; [0, 0, 0]];
